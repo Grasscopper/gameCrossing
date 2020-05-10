29e7c6a584152b6e080cebc10409d08ebe64f2c3
Grasscopper <jssstransky@gmail.com>
2020-05-10 11:26:46 -0400

    functional new point form on lists show page

--- app/javascript/react/containers/ListsShowContainer.js
+++ app/javascript/react/containers/ListsShowContainer.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
 
+import PointsNewComponent from "../components/PointsNewComponent"
+
 const ListsShowContainer = (props) => {
   let [list, setList] = useState({
     title: "",
@@ -8,6 +10,7 @@ const ListsShowContainer = (props) => {
     game: {}
   })
   let [points, setPoints] = useState([])
+  let [pointForm, setPointForm] = useState(false)
 
   let listID = props.match.params.id
   useEffect(() => {
@@ -33,6 +36,52 @@ const ListsShowContainer = (props) => {
     })
   }, [])
 
+  const postNewPoint = (formPayload) => {
+    fetch("/api/v1/points", {
+      credentials: "same-origin",
+      method: "POST",
+      body: JSON.stringify(formPayload),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    })
+    .then((response) => {
+      if (response.ok) {
+        return response
+      } else {
+        let errorMessage = `${response.status}: ${response.statusText}`
+        let error = new Error(errorMessage)
+        throw(error)
+      }
+    })
+    .then((response) => {
+      return response.json()
+    })
+    .then((body) => {
+      setPoints([
+        ...points,
+        {
+          title: body.title,
+          list_id: body.list_id
+        }
+      ])
+      setPointForm(false)
+    })
+    .catch((error) => {
+      console.error(`Error posting point: ${error.message}`)
+    })
+  }
+
+  const changePointForm = (event) => {
+    setPointForm(!pointForm)
+  }
+
+  let displayPointForm = <button onClick={changePointForm} className="new-list-buttons text-center">New point form</button>
+  if (pointForm) {
+      displayPointForm = <div><button onClick={changePointForm} className="new-list-buttons text-center">Back to list</button><PointsNewComponent key={list.id} list={list} postNewPoint={postNewPoint} /></div>
+  }
+
   let pointTiles = points.map((point) => {
     return (
       <p>{point.title}</p>
@@ -46,6 +95,9 @@ const ListsShowContainer = (props) => {
       </Link>
       <p>{list.title}</p>
       <img src={list.image} alt={list.title} />
+      <div className="text-center new-point-button">
+        {displayPointForm}
+      </div>
       <ul>
         {pointTiles}
       </ul>
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :games, only: [:index, :show, :create]
       resources :lists, only: [:show, :create]
+      resources :points, only: [:create]
     end
   end
 
