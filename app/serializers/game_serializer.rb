981f7d52b1f7cf14083b869d06c57e8d2a6d85e9
Grasscopper <jssstransky@gmail.com>
2020-05-13 16:21:25 -0400

    progress bar persists and works exactly like how it's supposed to

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,5 +1,5 @@
 class GameSerializer < ActiveModel::Serializer
-  attributes :id, :title, :image, :start_date, :time_played, :progress
+  attributes :id, :title, :image, :start_date, :time_played, :progress, :status
   belongs_to :user
   has_many :lists
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_07_165515) do
+ActiveRecord::Schema.define(version: 2020_05_13_180238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_05_07_165515) do
     t.string "time_played"
     t.string "progress"
     t.bigint "user_id"
+    t.string "status"
     t.index ["user_id"], name: "index_games_on_user_id"
   end
 
