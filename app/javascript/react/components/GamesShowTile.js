30ec801919e72602e5d334e2a5bed690c148bb37
Grasscopper <jssstransky@gmail.com>
2020-05-13 17:27:37 -0400

    qol

--- app/javascript/react/components/GamesShowTile.js
+++ app/javascript/react/components/GamesShowTile.js
@@ -5,8 +5,8 @@ const GamesShowTile = (props) => {
     <>
       <h1>{props.game.title}</h1>
       <img src={props.game.image} alt={props.game.title} />
-      <p id="game-show-padding-top"><p className="bold-game-show">Started Playing:</p> {props.game.start_date}</p>
-      <p><p className="bold-game-show">Time Played:</p> {props.game.time_played}</p>
+      <p id="game-show-padding-top" className="bold-game-show">Started Playing:</p><p >{props.game.start_date}</p>
+      <p className="bold-game-show">Time Played:</p><p>{props.game.time_played}</p>
       <p id="game-show-padding-bottom">{props.game.progress}</p>
     </>
   )
