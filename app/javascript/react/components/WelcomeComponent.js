f8f214666a9a1304d29c34db836120fa298ed93f
Grasscopper <jssstransky@gmail.com>
2020-05-15 18:04:20 -0400

    as per the last commit, removed borders and changed color for home page

--- app/javascript/react/components/WelcomeComponent.js
+++ app/javascript/react/components/WelcomeComponent.js
@@ -3,7 +3,7 @@ import React from 'react'
 const WelcomeComponent = (props) => {
   return (
     <div className="grid-container text-center" id="webpage-grid">
-      <div className="game-tiles">
+      <div>
         <h1>Welcome to Game Crossing!</h1>
       </div>
       <h1><a href="/users/sign_up">Sign up to start your video game collection</a></h1>
